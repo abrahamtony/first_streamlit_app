823b2b64d45179be1a9520cae7f2181efbc02c6a
abrahamtony <abrahamtony@gmail.com>
2022-10-06 18:53:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,6 @@ streamlit.text('Omega 3 & Blueberry Oat meal ')
 streamlit.text('Kale, Spinach & Rocket Smothie')
 streamlit.text('\N{chicken}Hard-boiled  Free-Range Egg')
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.multiselect("Pick some fruits", streamlit.list(my_fruit_list.index)
+streamlit.multiselect("Pick some fruits", streamlit.list(my_fruit_list.index))
+streamlit.dataframe(my_fruit_list)
 
