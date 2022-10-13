7a3405b9860733ea1bdc442e4a72da8d880babba
abrahamtony <abrahamtony@gmail.com>
2022-10-13 13:52:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ import pandas
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text(fruityvice_responsejson())
+streamlit.text(fruityvice_response.json())
