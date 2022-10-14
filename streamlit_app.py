8860030a76cd85996bec3283773c6020efa3b5a0
abrahamtony <abrahamtony@gmail.com>
2022-10-14 17:00:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@ import requests
 import streamlit
 import pandas
 streamlit.title("My Parents New Healthy Diner")
+streamlit.header("Breakfast Menu")
+streamlit.text('Omega 3 & Blueberry Oat meal ')
+streamlit.text('Kale, Spinach & Rocket Smothie')
+streamit.text('\N{chicken}Hard-boiled  Free-Range Egg')
